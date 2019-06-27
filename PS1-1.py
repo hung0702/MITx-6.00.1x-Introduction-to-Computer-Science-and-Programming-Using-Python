6a205a28064ee783abff4acdc3f7a3e581447579
hung0702 <Hung0702@gmail.com>
2019-06-27 01:37:31 -0400

    Update PS1-1.py

--- PS1-1.py
+++ PS1-1.py
@@ -1,4 +1,5 @@
-"""Assume 's' is a string of lower case characters. Write a program that
+"""
+Assume 's' is a string of lower case characters. Write a program that
 counts up the number of vowels contained in the string 's'
 """
 vowels = 0
